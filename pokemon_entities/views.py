93fe67fbc679ae6b87815773023e7384e5b4dcf1
Stranix <Phantom2525@gmail.com>
2023-04-11 11:11:49 +0300

    add en and jp pokemon title

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -73,6 +73,8 @@ def show_pokemon(request, pokemon_id):
     pokemon_for_template = {
         'img_url': request.build_absolute_uri(pokemon.image.url),
         'title_ru': pokemon.title,
+        'title_en': pokemon.title_en,
+        'title_jp': pokemon.title_jp,
         'description': pokemon.description
     }
 
